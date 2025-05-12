5354d1ca70eacc7bdc34b61f7c86727f8b78c9ac
paulornr89 <paulornr89@gmail.com>
2025-05-12 20:27:04 -0300

    login funcionando com pdo

--- projetoContinuoDeAula/validaLogin.php
+++ projetoContinuoDeAula/validaLogin.php
@@ -6,36 +6,28 @@ $_SESSION['usuarioLogado'] = false;
 $login = $_GET['login'];
 $senha = $_GET['senha'];
 
-if (!(empty($login) OR empty($senha))) // testa se os campos do formulário não estão vazios
-{
+if (!(empty($login) OR empty($senha))) {// testa se os campos do formulário não estão vazios
     include "conecta.php";
 
     $stmt = $pdo->query("select * from pessoa where email = '$login' and senha = '$senha'");
-    // $dados = array(':email' => $login, ':senha' => $senha);
-    $resultado = $stmt -> fetch();
-    // "select * from pessoa where email='$login' and senha = '$senha'";
     
-    // $resultado = mysqli_query($conexao, $sql);
+    if($stmt -> rowCount() == 1) {//verifica se a consulta retornou alguma linha
+        $resultado = $stmt -> fetch();
 
-    echo $resultado['email'];
-    
-    if(mysqli_num_rows($resultado)==1)
-    {
-    
-        $_SESSION['usuarioLogado'] = true;
-        $_SESSION['login'] = $login;
-        echo "Logou";
-        header("Location: index.php");
-    
-    }
-    else
-    {
-        echo "Não Logou";
-        header("Location: login.php");
-        $_SESSION['usuarioLogado'] = false;
+        if($resultado['email'] == $login){
+            $_SESSION['usuarioLogado'] = true;
+            $_SESSION['login'] = $login;
+            echo "Logou";
+            header("Location: index.php");
+        
+        } else {
+            echo "Não Logou";
+            header("Location: login.php");
+            $_SESSION['usuarioLogado'] = false;
+        }
     }
 
-}else {
+} else {
     echo "Não Logou 2";
     header("Location: login.php");
     $_SESSION['usuarioLogado'] = false;
